1a385ffe22c938dd09f320ae7976a7741632c316
mbti <yero2033@gmail.com>
2023-01-18 16:36:16 +0900

    카톡공유변경

--- js/share.js
+++ js/share.js
@@ -7,7 +7,7 @@ function shareKakao() {
           title: '나와 어울리는 꽃과 식물은?',
           description: '행복정원 MBTI 테스트',
           imageUrl:
-            './img/share.png',
+            '../img/share.png',
           link: {
             mobileWebUrl: 'https://developers.kakao.com',
             webUrl: 'https://developers.kakao.com',
@@ -15,34 +15,11 @@ function shareKakao() {
         },
         itemContent: {
           profileText: 'Kakao',
-          profileImageUrl: 'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
-          titleImageUrl: 'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
-          titleImageText: 'Cheese cake',
-          titleImageCategory: 'Cake',
-          items: [
-            {
-              item: 'Cake1',
-              itemOp: '1000원',
-            },
-            {
-              item: 'Cake2',
-              itemOp: '2000원',
-            },
-            {
-              item: 'Cake3',
-              itemOp: '3000원',
-            },
-            {
-              item: 'Cake4',
-              itemOp: '4000원',
-            },
-            {
-              item: 'Cake5',
-              itemOp: '5000원',
-            },
-          ],
-          sum: '총 결제금액',
-          sumOp: '15000원',
+          profileImageUrl: '../img/share.png',
+          titleImageUrl: '../img/share.png',
+          titleImageText: '나와 어울리는 꽃과 식물은?',
+          titleImageCategory: '행복정원 MBTI',
+
         },
         social: {
           likeCount: 10,
